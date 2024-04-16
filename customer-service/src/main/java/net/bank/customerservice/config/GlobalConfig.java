6cb532c405d8c36f8ffe17df56a53b104477cc17
zouhir <zouhir.etransit@gmail.com>
2024-04-16 11:28:56 +0100

    6 commit

--- customer-service/src/main/java/net/bank/customerservice/config/GlobalConfig.java
+++ customer-service/src/main/java/net/bank/customerservice/config/GlobalConfig.java
@@ -1,7 +1,15 @@
 package net.bank.customerservice.config;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @ConfigurationProperties(prefix = "global.params")
-public record GlobalConfig(int p1, int p2) {
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class GlobalConfig{
+    private int p1;
+    private int p2;
 }
